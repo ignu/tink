4c26873d5825451f9044f89702ff86f11ab3c7f0
Len Smith <len@barrison.com>
2017-07-08 09:20:30 -0700

    list tweets

--- index.js
+++ index.js
@@ -1,20 +1,63 @@
 const {h, mount, Component, Text} = require('ink');
 
+const Twitter = require("twitter")
+
+const {
+  consumer_key,
+  consumer_secret,
+  access_token_key,
+  access_token_secret } = process.env
+
+const client = new Twitter({
+  consumer_key,
+  consumer_secret,
+  access_token_key,
+  access_token_secret
+})
+
+
+const Tweet = (props) => {
+  return(
+    <div>
+      <Text green>@{props.user.screen_name}</Text><br/>
+      <Text>{props.text}</Text><br/><br/>
+    </div>
+  )
+}
+
 class Tink extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      tweets: []
+      tweets: [],
+      error: null
     }
+
+    client.get('statuses/user_timeline', (err, t) => {
+      if(err) {
+        const message = err[0] ? err[0].message : err
+        console.log("😎 err", err);
+        this.setState({
+          tweets: [],
+          error: `ERROR: '${message}'`
+        })
+      } else {
+        this.setState({
+          tweets: t,
+          error: null
+        })
+      }
+    })
   }
 
   render() {
     return (
-      <Text green>
-        {this.state.tweets.length}
+      <div>
+        <Text red>{this.state.error}</Text>
+        {this.state.tweets.map(t => <Tweet {...t} />)}
         <br/>
-      </Text>
+      </div>
     );
   }
 }
@@ -24,4 +67,4 @@ const unmount = mount(<Tink/>, process.stdout);
 setTimeout(() => {
   console.log("😎 goodbye.");
   unmount();
-}, 10000);
+}, 4000);
