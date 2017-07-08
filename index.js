baf1dc361e5efa51b133bd9184ba07a8d1774e15
Len Smith <len@barrison.com>
2017-07-08 13:18:27 -0700

    use dotenv

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
+require('dotenv').config()
 const {h, mount, Component, Text} = require('ink');
 
 const Twitter = require("twitter")
 
-const {
-  consumer_key,
-  consumer_secret,
-  access_token_key,
-  access_token_secret } = process.env
-
 const client = new Twitter({
-  consumer_key,
-  consumer_secret,
-  access_token_key,
-  access_token_secret
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret : process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 })
-
 
 const Tweet = (props) => {
   return(
